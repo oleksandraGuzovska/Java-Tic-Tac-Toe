56ebdb407e61a40eb382bbe4fc208f9730734486
Guzovska, Oleksandra <39601016+oleksandraGuzovska@users.noreply.git.hub.com>
2019-04-11 09:12:13 -0600

    Removed Path

---
+++ src/ca/cmpt305/TicTacToe/SavedMoves.java
@@ -0,0 +1,15 @@
+package ca.cmpt305.TicTacToe;
+
+public class SavedMoves {
+	
+	Player player;
+	int row;
+	int col;
+	
+	public SavedMoves(Player player, int row, int col) {
+		this.player = player;
+		this.row = row;
+		this.col = col;
+	}
+
+}
